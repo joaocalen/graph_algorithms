6f15f701e75d5c37e74cbbf0f58f51112b2e1343
Joao Gobeti <joao.gobeti@motora.ai>
2023-07-16 13:51:44 -0300

    changing dijkstra to heap and adding kruskal

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Graph:
     def __init__(self):
         self.vertices = {}  # Dictionary to store the vertices and their connections
@@ -16,23 +18,96 @@ class Graph:
             neighbors_str = ", ".join([f"{neighbor} ({weight})" for neighbor, weight in neighbors.items()])
             graph_str += f"{vertex}: {neighbors_str}\n"
         return graph_str
-    
+
     def dijkstra(self, start_vertex):
         distances = {vertex: float('inf') for vertex in self.vertices}  # Initialize distances to all vertices as infinity
         distances[start_vertex] = 0  # Set the distance to the start_vertex as 0
+        heap = [(0, start_vertex)]  # Create a priority queue (heap) with initial distance and start_vertex
+        heapq.heapify(heap)
         visited = set()
-        unvisited = list(self.vertices.keys())
         
-        while unvisited:
-            # Find the vertex with the minimum distance among the unvisited vertices
-            current_vertex = min(unvisited, key=lambda vertex: distances[vertex])
-            unvisited.remove(current_vertex)
+        while heap:
+            current_distance, current_vertex = heapq.heappop(heap)  # Get the vertex with the smallest distance
             visited.add(current_vertex)
             
             for neighbor, weight in self.vertices[current_vertex].items():
-                if neighbor in unvisited:
-                    distance = distances[current_vertex] + weight
-                    if distance < distances[neighbor]:
-                        distances[neighbor] = distance
+                distance = current_distance + weight
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    heapq.heappush(heap, (distance, neighbor))
         
         return distances
+
+    def find(self, parent, vertex):
+        if parent[vertex] == vertex:
+            return vertex
+        return self.find(parent, parent[vertex])
+    
+    def union(self, parent, rank, vertex1, vertex2):
+        root1 = self.find(parent, vertex1)
+        root2 = self.find(parent, vertex2)
+        
+        if rank[root1] < rank[root2]:
+            parent[root1] = root2            
+        elif rank[root1] > rank[root2]:
+            parent[root2] = root1            
+        else:
+            parent[root2] = root1
+            rank[root1] += 1
+
+    def kruskal(self):
+        parent = {}  # Dictionary to store the parent of each vertex for finding cycles
+        rank = {}  # Dictionary to store the rank of each vertex for union by rank
+        
+        for vertex in self.vertices:
+            parent[vertex] = vertex
+            rank[vertex] = 0
+        
+        edges = []
+        for start_vertex, connections in self.vertices.items():
+            for end_vertex, weight in connections.items():
+                edges.append((start_vertex, end_vertex, weight))
+        
+        edges.sort(key=lambda x: x[2])  # Sort the edges in ascending order based on weight        
+        minimum_spanning_tree = Graph()  # Graph to store the minimum spanning tree
+        
+        for edge in edges:
+            # print("For edge: " + str(edge))
+            start_vertex, end_vertex, weight = edge
+            
+            root1 = self.find(parent, start_vertex)
+            root2 = self.find(parent, end_vertex)
+            # print("root 1: " + str(root1) + " rank: " + str(rank[root1]) + ", root 2: " + str(root2) + " rank: " + str(rank[root2]))
+
+            
+            if root1 != root2:  # If adding the edge does not form a cycle
+                if start_vertex not in minimum_spanning_tree.vertices:
+                    minimum_spanning_tree.add_vertex(start_vertex)                
+                if end_vertex not in minimum_spanning_tree.vertices:
+                    minimum_spanning_tree.add_vertex(end_vertex)
+                minimum_spanning_tree.add_edge(start_vertex, end_vertex, weight)                
+                
+                self.union(parent, rank, root1, root2)
+        
+        return minimum_spanning_tree
+    
+
+# Outside the class
+
+def read_graph_file(file_path):
+    graph = Graph()
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('c'):
+                continue  # Skip comment lines
+            elif line.startswith('p'):
+                _, _, num_vertices, num_edges = line.split()
+                num_vertices = int(num_vertices)
+                num_edges = int(num_edges)
+                for vertex in range(1, num_vertices + 1):
+                    graph.add_vertex(str(vertex))
+            elif line.startswith('a'):
+                _, start_vertex, end_vertex, weight = line.split()
+                graph.add_edge(start_vertex, end_vertex, int(weight))
+    return graph
--- main.py
+++ main.py
@@ -1,29 +1,51 @@
 import graph as gr
+import sys
 
 # Create a new graph
-graph = gr.Graph()
+# graph = gr.Graph()
+
+file_path = sys.argv[1]  # Path to the input graph file
+graph = gr.read_graph_file(file_path)
 
 # Add vertices
-graph.add_vertex("A")
-graph.add_vertex("B")
-graph.add_vertex("C")
-graph.add_vertex("D")
-graph.add_vertex("E")
+# graph.add_vertex("A")
+# graph.add_vertex("B")
+# graph.add_vertex("C")
+# graph.add_vertex("D")
+# graph.add_vertex("E")
+# graph.add_vertex("F")
+# graph.add_vertex("G")
 
-# Add weighted edges
-graph.add_edge("A", "B", 4)
-graph.add_edge("A", "C", 2)
-graph.add_edge("B", "C", 1)
-graph.add_edge("B", "D", 5)
-graph.add_edge("C", "D", 8)
-graph.add_edge("C", "E", 10)
-graph.add_edge("D", "E", 2)
+# # Add weighted edges
+# graph.add_edge("A", "B", 7)
+# graph.add_edge("A", "D", 5)
+# graph.add_edge("B", "C", 8)
+# graph.add_edge("B", "D", 9)
+# graph.add_edge("B", "E", 7)
+# graph.add_edge("C", "E", 5)
+# graph.add_edge("D", "E", 15)
+# graph.add_edge("D", "F", 6)
+# graph.add_edge("E", "F", 8)
+# graph.add_edge("E", "G", 9)
+# graph.add_edge("F", "G", 11)
+
 
 # Apply Dijkstra's algorithm
-start_vertex = "A"
+start_vertex = '1'
 distances = graph.dijkstra(start_vertex)
 
-# Print the shortest distances from the start_vertex
+# # Print the shortest distances from the start_vertex
 for vertex, distance in distances.items():
     print(f"Shortest distance from {start_vertex} to {vertex}: {distance}")
 print(graph)
+
+
+
+
+# minimum_spanning_tree = graph.kruskal()
+# print(minimum_spanning_tree)
+
+# Print the minimum spanning tree
+# for vertex, connections in minimum_spanning_tree.vertices.items():
+#     for neighbor, weight in connections.items():
+#         print(f"Edge: {vertex} - {neighbor}, Weight: {weight}")
