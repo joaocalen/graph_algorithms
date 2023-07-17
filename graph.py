601755c1a5283ce0cb3530510fdf44cae309cc52
Joao Gobeti <joao.gobeti@motora.ai>
2023-07-16 21:21:48 -0300

    THANK GOD IT'S WORKING

--- graph.py
+++ graph.py
@@ -1,8 +1,10 @@
 import heapq
+from collections import deque
 
 class Graph:
-    def __init__(self):
+    def __init__(self, directed=False):
         self.vertices = {}  # Dictionary to store the vertices and their connections
+        self.directed = directed # True if it's a directed graph. False otherwise.
     
     def add_vertex(self, vertex):
         self.vertices[vertex] = {}  # Initialize an empty dictionary to store the connections and their weights
@@ -10,7 +12,10 @@ class Graph:
     def add_edge(self, start_vertex, end_vertex, weight):
         if start_vertex in self.vertices and end_vertex in self.vertices:
             self.vertices[start_vertex][end_vertex] = weight  # Add the weighted edge from start_vertex to end_vertex
-            self.vertices[end_vertex][start_vertex] = weight  # Add the weighted edge from end_vertex to start_vertex
+            if not self.directed:
+                self.vertices[end_vertex][start_vertex] = weight  # Add the weighted edge from end_vertex to start_vertex
+            else:
+                self.vertices[end_vertex][start_vertex] = 0  # Add the weighted edge from end_vertex to start_vertex
     def __str__(self):
         graph_str = ""
         for vertex in self.vertices:
@@ -91,23 +96,117 @@ class Graph:
         
         return minimum_spanning_tree
     
+class MaxFlowGraph(Graph):
+    def __init__(self):
+        super().__init__(directed=True)
+        self.s = None  # Source vertex
+        self.t = None  # Sink vertex
+
+    def set_source(self, source):
+        self.s = source
+
+    def set_sink(self, sink):
+        self.t = sink
+    
+    def bfs(self, parent):
+        visited = set()
+        queue = deque()
+
+        # Start from the source vertex
+        visited.add(self.s)
+        queue.append(self.s)
+
+        while queue:
+            current_vertex = queue.popleft()
+            for neighbor, capacity in self.residual_graph.vertices[current_vertex].items():                
+                if neighbor not in visited and capacity > 0:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+                    parent[neighbor] = current_vertex
+
+                    # Stop BFS if the sink vertex is reached
+                    if neighbor == self.t:
+                        return True
+
+        return False
+
+    def ford_fulkerson_algorithm(self):
+        self.residual_graph = MaxFlowGraph()
+        for vertex in self.vertices:
+            self.residual_graph.add_vertex(vertex)
+        for vertex in self.vertices:
+            for neighbor, capacity in self.vertices[vertex].items():
+                if capacity > 0:
+                    self.residual_graph.add_edge(vertex, neighbor, capacity)
+        
+        max_flow = 0
+        parent = {}
+
+        while self.bfs(parent):
+            # Find the minimum capacity along the augmenting path
+            path_flow = float('inf')
+            current_vertex = self.t
+
+            while current_vertex != self.s:
+                previous_vertex = parent[current_vertex]
+                path_flow = min(path_flow, self.residual_graph.vertices[previous_vertex][current_vertex])
+                current_vertex = previous_vertex
+
+            # Update the residual capacities and reverse edges along the augmenting path
+            current_vertex = self.t
+
+            while current_vertex != self.s:
+                previous_vertex = parent[current_vertex]
+                self.residual_graph.vertices[previous_vertex][current_vertex] -= path_flow
+                self.residual_graph.vertices[current_vertex][previous_vertex] += path_flow
+                current_vertex = previous_vertex
+
+            max_flow += path_flow
+
+        print("Maximum Flow Graph:")
+        print(self)
+        print("Residual Graph:")
+        print(self.residual_graph)
+        flow_values = {}
+        for vertex, connections in self.vertices.items():
+            flow_values[vertex] = {}
+
+            for neighbor, _ in connections.items():
+                if self.vertices[vertex][neighbor] > 0:
+                    flow_values[vertex][neighbor] = (
+                        self.vertices[vertex][neighbor] - self.residual_graph.vertices[vertex][neighbor]
+                    )
+
+        return max_flow, flow_values
 
 # Outside the class
 
 def read_graph_file(file_path):
-    graph = Graph()
+    graph = None
     with open(file_path, 'r') as file:
         for line in file:
             line = line.strip()
             if line.startswith('c'):
                 continue  # Skip comment lines
             elif line.startswith('p'):
-                _, _, num_vertices, num_edges = line.split()
+                _, type, num_vertices, num_edges = line.split()                
                 num_vertices = int(num_vertices)
                 num_edges = int(num_edges)
-                for vertex in range(1, num_vertices + 1):
+                if type == "max":
+                    graph = MaxFlowGraph() # Define the graph to a directed graph
+                else:
+                    graph = Graph()
+                for vertex in range(1, num_vertices + 1):                    
                     graph.add_vertex(str(vertex))
             elif line.startswith('a'):
                 _, start_vertex, end_vertex, weight = line.split()
                 graph.add_edge(start_vertex, end_vertex, int(weight))
+            elif line.startswith('n'):
+                _, vertex_id, vertex_type = line.split()
+                vertex_id = str(vertex_id)
+                if vertex_type == 's':
+                    graph.set_source(vertex_id)
+                elif vertex_type == 't':
+                    graph.set_sink(vertex_id)
+                
     return graph
--- main.py
+++ main.py
@@ -7,37 +7,14 @@ import sys
 file_path = sys.argv[1]  # Path to the input graph file
 graph = gr.read_graph_file(file_path)
 
-# Add vertices
-# graph.add_vertex("A")
-# graph.add_vertex("B")
-# graph.add_vertex("C")
-# graph.add_vertex("D")
-# graph.add_vertex("E")
-# graph.add_vertex("F")
-# graph.add_vertex("G")
-
-# # Add weighted edges
-# graph.add_edge("A", "B", 7)
-# graph.add_edge("A", "D", 5)
-# graph.add_edge("B", "C", 8)
-# graph.add_edge("B", "D", 9)
-# graph.add_edge("B", "E", 7)
-# graph.add_edge("C", "E", 5)
-# graph.add_edge("D", "E", 15)
-# graph.add_edge("D", "F", 6)
-# graph.add_edge("E", "F", 8)
-# graph.add_edge("E", "G", 9)
-# graph.add_edge("F", "G", 11)
-
-
 # Apply Dijkstra's algorithm
-start_vertex = '1'
-distances = graph.dijkstra(start_vertex)
+# start_vertex = '1'
+# distances = graph.dijkstra(start_vertex)
 
 # # Print the shortest distances from the start_vertex
-for vertex, distance in distances.items():
-    print(f"Shortest distance from {start_vertex} to {vertex}: {distance}")
-print(graph)
+# for vertex, distance in distances.items():
+#     print(f"Shortest distance from {start_vertex} to {vertex}: {distance}")
+# print(graph)
 
 
 
@@ -49,3 +26,14 @@ print(graph)
 # for vertex, connections in minimum_spanning_tree.vertices.items():
 #     for neighbor, weight in connections.items():
 #         print(f"Edge: {vertex} - {neighbor}, Weight: {weight}")
+
+# Apply the Ford-Fulkerson algorithm
+max_flow, flow_values = graph.ford_fulkerson_algorithm()
+
+# Print the maximum flow
+print("Maximum Flow:", max_flow)
+
+# Print the flow values for each edge
+for vertex, connections in flow_values.items():
+    for neighbor, flow in connections.items():
+        print(f"Edge: {vertex} - {neighbor}, Flow: {flow}")
